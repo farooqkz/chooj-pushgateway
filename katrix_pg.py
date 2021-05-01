cfdf2040139f855999225286a6630353f88c2866
Farooq Karimi Zadeh <farooqkz@hashbang.sh>
2021-05-01 15:21:35 +0200

    adding log file and fix

--- katrix_pg.py
+++ katrix_pg.py
@@ -7,24 +7,28 @@ import sys
 
 app = Flask(__name__)
 
+LOG_FILE = open("log", "at")
+
 def send_request(pushkey, data=None):
     if not pushkey.startswith("https://push.kaiostech.com:8443/wpush"):
+        print("pushkey is", pushkey, "ignoring...", file=LOG_FILE)
         return False
     try:
         response = requests.post(pushkey, data=data)
     except:
         return False
     if not response.ok:
-        print(response.text, file=sys.stderr)
+        print(response.text, file=LOG_FILE)
     return False if response.ok else pushkey
 
-@app.route("/notify", methods=("POST", ))
+@app.route("/_matrix/push/v1/notify", methods=("POST", ))
 def notify():
     if not request.is_json:
+        print("Request is not JSON:", request, file=LOGFILE)
         abort(404)
-    pushkeys = map(
+    pushkeys = tuple(map(
         lambda device: device["pushkey"],
         request.json["notification"]["devices"]
-    )
-    rejected = filter(None, map(send_request, zip(pushkeys, request.json))
+    ))
+    rejected = filter(None, map(send_request, pushkeys, (request.json, ) * len(pushkeys)))
     return {"rejected": list(rejected)}
