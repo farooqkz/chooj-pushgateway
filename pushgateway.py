494002939c2b3127ee79ba1fb539b7a7b569fa11
Farooq Karimi Zadeh <fkz@riseup.net>
2021-12-08 19:40:54 +0330

    some cleanup and improvement

--- pushgateway.py
+++ pushgateway.py
@@ -1,34 +1,35 @@
-from flask import Flask # <3?
+from flask import Flask  # <3?
 from flask import request
 from flask import abort
-from flask import g
 import requests
-import sys
 
 app = Flask(__name__)
 
-LOG_FILE = open("log", "at")
 
 def send_request(pushkey, data=None):
     if not pushkey.startswith("https://push.kaiostech.com:8443/wpush"):
-        print("pushkey is", pushkey, "ignoring...", file=LOG_FILE)
         return False
     try:
         response = requests.post(pushkey, data=data)
     except:
         return False
-    if not response.ok:
-        print(response.text, file=LOG_FILE)
     return False if response.ok else pushkey
 
-@app.route("/_matrix/push/v1/notify", methods=("POST", ))
+
+@app.route("/_matrix/push/v1/notify", methods=("POST",))
 def notify():
     if not request.is_json:
-        print("Request is not JSON:", request, file=LOGFILE)
         abort(404)
-    pushkeys = tuple(map(
+    if not isinstance(request.json.get("notification"), dict):
+        abort(400)
+
+    pushkeys = map(
         lambda device: device["pushkey"],
-        request.json["notification"]["devices"]
-    ))
-    rejected = filter(None, map(send_request, pushkeys, (request.json, ) * len(pushkeys)))
-    return {"rejected": list(rejected)}
+        request.json["notification"]["devices"],
+    )
+    request.json["notification"].pop("devices")
+    rejected = list()
+    for pushkey in pushkeys:
+        if not send_request(pushkey, request.json["notification"]):
+            rejected.append(pushkey)
+    return {"rejected": rejected}
