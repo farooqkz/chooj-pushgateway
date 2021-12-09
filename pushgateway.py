8ab02be68c486153bff0d6bc818ae8dcc222ae41
Farooq Karimi Zadeh <farooqkz@hashbang.sh>
2021-12-09 16:21:16 +0100

    finally working with Vapid!

--- pushgateway.py
+++ pushgateway.py
@@ -1,20 +1,53 @@
 from flask import Flask  # <3?
 from flask import request
 from flask import abort
+from flask import g
+from pywebpush import webpush
+from pywebpush import WebPushException
+from py_vapid import Vapid01
 import requests
+import json
 
 app = Flask(__name__)
 
+def get_vapid():
+    vapid = getattr(g, "vapid", None)
+    if vapid is None:
+        pub = None
+        priv = None
+        with open(".vapid") as fp:
+            pub = fp.readline()
+            priv = fp.readline()
+        priv = Vapid01.from_string(priv)
+        vapid = dict(pub=pub, priv=priv)
+        setattr(g, "vapid", vapid)
+    return vapid
 
-def send_request(pushkey, data=None):
-    if not pushkey.startswith("https://push.kaiostech.com:8443/wpush"):
+def push(subscription, data):
+    endpoint = subscription.get("endpoint", "")
+    if not endpoint.startswith("https://push.kaiostech.com:8443/wpush"):
+        print("Invalid endpoint:", endpoint)
         return False
+    privkey = get_vapid()["priv"]
+    vapid_claims = dict(sub="mailto:fk" + "z" + "@riseup.net")
     try:
-        response = requests.post(pushkey, data=data)
-    except:
+        webpush(
+            subscription_info=subscription,
+            data=json.dumps(data),
+            vapid_private_key=privkey,
+            vapid_claims=vapid_claims,
+            content_encoding="aesgcm",
+            ttl=120,
+        )
+    except WebPushException as e:
+        print(e)
+        if e.response and e.response.json():
+            print(e.response.json())
         return False
-    return False if response.ok else pushkey
-
+    except Exception as e:
+        print(e)
+        return False
+    return True
 
 @app.route("/_matrix/push/v1/notify", methods=("POST",))
 def notify():
@@ -22,7 +55,7 @@ def notify():
         abort(404)
     if not isinstance(request.json.get("notification"), dict):
         abort(400)
-
+    
     pushkeys = map(
         lambda device: device["pushkey"],
         request.json["notification"]["devices"],
@@ -30,6 +63,11 @@ def notify():
     request.json["notification"].pop("devices")
     rejected = list()
     for pushkey in pushkeys:
-        if not send_request(pushkey, request.json["notification"]):
+        try:
+            subscr = json.loads(pushkey)
+            if not push(subscr, request.json["notification"]):
+                rejected.append(pushkey)
+        except:
             rejected.append(pushkey)
+    print("REJECTED:", rejected)
     return {"rejected": rejected}
